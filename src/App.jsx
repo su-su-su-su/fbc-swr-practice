07ba5731d3327439aa98b689a54f17c990f9eff0
su-su-su-su <suzukikoji0503@gmail.com>
2024-03-27 00:32:10 +0900

    SWRを使ったコードに変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
 import "./App.css";
+import useSWR from "swr";
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
 
-  const [status, setStatus] = useState("");
+  const fetcher = (url) => fetch(url, { headers }).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(url, fetcher);
 
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
 
-  return <>{status && <p>Status : {status}</p>}</>;
+  return <>{data && <p>Status : {data.description}</p>}</>;
 }
 
 export default App;
